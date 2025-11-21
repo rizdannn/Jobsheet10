80ba1853cb9eea89a4f715ef59d80adfad55e7bf
rizdannn <rafifprastana@gmail.com>
2025-11-21 10:58:26 +0700

    Modifikasi Percobaan 1

--- Bioskop22.java
+++ Bioskop22.java
@@ -2,7 +2,7 @@ public class Bioskop22 {
     public static void main(String[] args) {
 
         String penonton[][] = new String[4][2];
-        
+
         penonton[0][0] = "Amin";
         penonton[0][1] = "Bena";
         penonton[1][0] = "Candra";
@@ -10,10 +10,46 @@ public class Bioskop22 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
 
+        // Cetak manual
         System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
+
+        // Panjang array 2D
+        System.out.println(penonton.length);
+        System.out.println(penonton[0].length);
+        System.out.println(penonton[1].length);
+        System.out.println(penonton[2].length);
+        System.out.println(penonton[3].length);
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + penonton[i].length);
+        }
+
+        System.out.println(penonton.length);
+
+        // Enhanced for
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+
+        // Penonton baris ke-3
+        System.out.println("Penonton pada baris ke 3: ");
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
+
+        for (String i : penonton[2]) {
+            System.out.println(i);
+        }
+
+        // Menampilkan semua baris
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton baris ke " + (i + 1) + ": "
+                    + String.join(",", penonton[i]));
+        }
     }
 }
